f23e2d10aa293140b0a699acd54296a5a56d855a
Wei Lun <garyganweilun@gmail.com>
2021-11-13 18:32:56 +0800

    Refactor Coin details

--- src/Components/CoinDetails.jsx
+++ src/Components/CoinDetails.jsx
@@ -1,25 +1,35 @@
-import { Layout, Typography } from 'antd';
 import { useParams } from 'react-router';
 import React, { useEffect, useState } from 'react';
-import CoinData from './CoinData';
+import CoinResults from './CoinResults';
+let today=Date.now();
 
 
-
-
+console.log(today)
 
 
 function CoinDetails() {
     const { id } = useParams();
     const coingeckoEndpoint = `https://api.coingecko.com/api/v3/coins/${id}?tickers=false&market_data=true`
+    const chartPriceEndpoint=`https://api.coingecko.com/api/v3/coins/bitcoin/market_chart/range?vs_currency=usd&from=1609430400&to=${today}`
 
     const [getCoinData, setCoinData] = useState([]);
+    const [getChartData, setChartData] = useState([]);
 
     const fetchCoin = async () => {
         try {
             const response = await fetch(coingeckoEndpoint);
             const data = await response.json();
             setCoinData([data])
-            console.log(getCoinData)
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    const fetchChart = async () => {
+        try {
+            const response = await fetch(chartPriceEndpoint);
+            const data = await response.json(response);
+            setChartData([data.prices])
         } catch (err) {
             console.log(err);
         }
@@ -31,9 +41,15 @@ function CoinDetails() {
         return () => { setCoinData([]) };
     }, []);
 
+    useEffect(() => {
+        fetchChart();
+        return () => { setChartData([]) };
+    }, []);
+
+
     return (
 
-        <CoinData fetched={getCoinData} />
+        <CoinResults fetchedDetails={getCoinData} fetchChart={getChartData}/>
 
 
 
